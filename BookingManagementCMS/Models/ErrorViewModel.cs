74db2f6aa50dc79a389d24efa0009bc52c5ca362
venubabu <venuinfo.s@gmail.com>
2025-07-15 12:58:44 +0530

    Added default code project

---
+++ BookingManagementCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingManagementCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
