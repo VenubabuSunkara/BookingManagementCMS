516d49817ea24af77a5d699e6dad56ab4f86e5ee
venubabu <venuinfo.s@gmail.com>
2025-08-04 16:35:26 +0530

    Fixed the issues

--- Booking.Web/Controllers/DriverController.cs
+++ Booking.Web/Controllers/DriverController.cs
@@ -15,6 +15,36 @@ namespace Booking.Web.Controllers
             _logger = logger;
             _driverService = driverService;
         }
+          [HttpPost]
+        //public async Task<IActionResult> LoadDriverData(DatatableRequest request, CancellationToken cancellationToken)
+        //{
+        //    var result = await _driverService.GetAllAsync();
+        //    return Json(new
+        //    {
+        //        draw = result.draw,
+        //        recordsFiltered = result.recordsFiltered,
+        //        recordsTotal = result.recordsTotal,
+        //        data = result?.data?.Select(x => new
+        //        {
+        //            x.VehicleName,
+        //            x.Capacity,
+        //            Photo = x.DriverVehicleMedia
+        //                     .FirstOrDefault(m => m.IsDefault)?
+        //                     .MediaValue,
+        //            VehicleType = x.VehicleType,
+        //            DriverName = x.DriverFirstName + " " + x.DriverLastName,
+        //            Contact = x.DriverContact,
+        //            CreatedDate = x.CreatedOn,
+        //            x.Id
+        //        }).ToArray()
+        //    });
+        //}
+        public async Task<int> Approve(int DriverVehileId)
+        {
+            if (DriverVehileId == 0)
+                throw new ArgumentException();
+            return await _driverService.ApproveDriverAsync(DriverVehileId);
+        }
 
         public async Task<IActionResult> Index()
         {
