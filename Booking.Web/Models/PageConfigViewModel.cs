7a7d59b2b3d115bbbbea6ff084e9d4e4472661f1
Venubabu [C] Sunkara <sunkvenu@amazon.com>
2025-10-07 15:04:08 +0530

    Page Configuration Add

---
+++ Booking.Web/Models/PageConfigViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Booking.Web.Models
+{
+    public class PageConfigViewModel
+    {
+        public string PageName { get; init; } = null!;
+        public string PageContentData { get; init; } = null!;
+        public bool IsActive { get; init; }
+        public string Placeholder { get; init; } = null!;
+        public List<SelectListItem>  PageNames { get; set; } = [];
+        public int? Id { get; set; }
+    }
+}
